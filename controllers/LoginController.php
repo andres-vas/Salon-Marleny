9080d1c99d9f8d02f0f2721fd5d5210b504e979a
Victor Andres Vasquez Hernandez <andres07vas@gmail.com>
2024-09-11 21:45:51 -0600

    CREACION DEL ROUTING Y CONTROLADOR
    Se crearon las rutas y controlador para el inicio de sesión, cerrar sesión, olvide cuenta, recuperar contraseña, crear cuentas.

---
+++ controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Controllers;
+
+
+class LoginController {
+    public static function login() {
+        echo "DESDE LOGIN";
+    }
+
+    public static function logout() {
+        echo "DESDE LOGOUT";
+    }
+
+    public static function olvide() {
+        echo "DESDE OLVIDE";
+    }
+
+    public static function recuperar() {
+        echo "DESDE RECUPERAR";
+    }
+
+    public static function crear() {
+        echo "DESDE CREAR";
+    }
+
+}

86cc63866630b56549872e014953666498c7b782
Victor Andres Vasquez Hernandez <andres07vas@gmail.com>
2024-09-24 21:54:23 -0600

    Finalizacion del layout
    Finalzacion para la pagina

--- views/layout.php
+++ views/layout.php
@@ -5,24 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>App Salón</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="build/css/app.css">
+    <link rel="stylesheet" href="/build/css/app.css">
 </head>
 <body>
-
     <div class="contenedor-app">
-        <!-- ---COLOCAREMOS LA IMAGEN QUE SE UTILIZARA EN CSS--- -->
         <div class="imagen"></div>
-
-        <!-- ---SE MANDA A LLAMAR EL CONTENIDO DE LA APLICACION--- -->
         <div class="app">
             <?php echo $contenido; ?>
         </div>
     </div>
 
-
-
-
-    
+    <?php
+        echo $script ?? '';
+    ?>
             
 </body>
 </html>

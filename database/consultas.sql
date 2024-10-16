0bbf5d32b9c8acd1982391275826cda9da7b5037
Victor Andres Vasquez Hernandez <andres07vas@gmail.com>
2024-10-16 00:01:08 -0600

    CONSULTAS

--- database/consultas.sql
+++ database/consultas.sql
@@ -1,3 +1,4 @@
+USE salon_marleny;
 SELECT * FROM usuarios;
 
 CREATE TABLE marca (
@@ -24,6 +25,20 @@ CREATE TABLE promocion (
 	activo_descuento VARCHAR(1) DEFAULT 'A',
     CONSTRAINT fk_descuento_promocion FOREIGN KEY (id_tipo_descuento) REFERENCES tipo_descuento(id)
 );
-SELECT * FROM promocion
+SELECT * FROM promocion;
 
-
+CREATE TABLE producto (
+	id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    name_producto VARCHAR(300),
+    id_marca_producto INT(11) ,
+	id_promocion_producto INT(11),
+	precio_producto DECIMAL(5,2),
+	descripcion_producto VARCHAR(300),
+	fecha_creacion_producto DATE,
+	fecha_modificado_producto DATE,
+	stock_producto NUMERIC(11),
+	CONSTRAINT fk_producto_marca FOREIGN KEY (id_marca_producto) REFERENCES marca(id),
+    CONSTRAINT fk_producto_promocion FOREIGN KEY (id_promocion_producto) REFERENCES promocion(id)
+);
+SELECT * FROM producto;
+drop table producto
